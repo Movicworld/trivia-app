8b5ddc9a1bd74de74e0cbb5513014bebe8f2053c
Victor Morakinyo <morakinyovictor1@gmail.com>
2025-04-15 15:19:55 +0100

    Assets added

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,5 +6,10 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+{{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+{{-- <link rel="stylesheet" href="{{ asset('../css/app.css') }}"> --}}
+<link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+<script src="{{ asset('assets/js/app.js') }}"></script>
+
+
 @fluxAppearance
